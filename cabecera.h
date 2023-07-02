207a22880840383893eee7405407dc5b1879041d
Joaquin Gavernet <joaquingavernet@gmail.com>
2023-07-01 21:22:32 -0300

    Middle step jaja.

--- cabecera.h
+++ cabecera.h
@@ -3,7 +3,7 @@
 
   #include <stdio.h>
   #include <stdlib.h>
-  #include <math.h>
+  #include <time.h>
 
   // Se definen constantes de dimension para operar con matrices en memoria
   #ifndef DIM
@@ -12,7 +12,8 @@
   #endif
 
   // Inicializamos la matriz de key/llave para encriptar y desencriptar mensajes (alcance global)
-  int matrizKey[DIM][DIM] = {{4, 1, 20}, {2, 1, 0}, {0, 1, 5}};
+  int matrizKey[DIM][DIM] = {{7, 0, 0}, {0, 1, 1}, {0, 8, 9}};
+  int matrizKeyInvserse[DIM][DIM] = {{73, 0, 0}, {0, 9, 254}, {0, 247, 1}};
 
   // Se definen los tipos de datos utilizados
   typedef struct {
@@ -22,20 +23,12 @@
   } tuple;
 
   // Se definen macros utilizadas en las funciones de matrices
-  #define forn(i, n) for (i = 0; i < n; i++)
+  #define forn(i, n) for (int i = 0; i < n; i++)
 
   // Se incluyen los prototipos del archivo "cabecera.h"
   void printMatrix (int matrix[][DIM], int m, int n);
   void printMatrixFloat (float matrix[][DIM], int m, int n);
-  int sumaDefinida(int am, int an, int bm, int bn);
-  void sumarMatrices (const int A[][DIM], const int B[][DIM], int C[][DIM], int m, int n);
-  int * productoDefinido (int am, int an, int bm, int bn);
-  int * productoMatrices (int a[][DIM], int b[][DIM], int result[DIM][DIM], int am, int an, int bm, int bn);
-  int * submatriz(int a[][DIM], int x, int y, int n);
-  int determinante (int a[][DIM], int n);
-  int * calcularInversa (int a[][DIM], float result[][DIM], int n);
-  void permutacion (int arr[][DIM], int first, int second, int n);
-  void eliminacionGaussiana (int arr[][DIM], int m, int n);
-  int esCompatible (int a[][DIM], int m, int n);
+  // int * productoMatrices (int a[][DIM], int b[][DIM], int am, int an, int bm, int bn);
+  void delay(int);
 
 #endif
--- main.c
+++ main.c
@@ -5,11 +5,37 @@
 // Set DEBUG to 1 to print debug messages
 #define DEBUG 1
 
+int * productoMatrices (int a[][DIM], int b[][DIM], int am, int an, int bm, int bn) {
+  int * result = calloc((am * bn), sizeof(int));
+
+  int i = 0, j = 0;
+  while (i < am) {
+    int k = 0;
+    while (k < an) {
+      result[(i*am) + j] += a[i][k] * b[k][j];
+      k++;
+    }
+    if (j < an) j++;
+    else {
+      j = 0;
+      i++;
+    }
+  }
+
+  return result;
+}
+
 tuple hillCypher (tuple actual) {
   #if DEBUG
     printf("Tuple in hillCypher: %c%c%c\n", actual.first, actual.second, actual.third);
   #endif
-  productoMatrices((int *) &actual, matrizKey, (int *) &actual, 1, 3, 3, 3);
+  int input[1][3] = {{actual.first, actual.second, actual.third}};
+  int * result = calloc(1*3, sizeof(int));
+  result = productoMatrices(input, matrizKey, 1, 3, 3, 3);
+
+  actual.first = result[0] % MOD;
+  actual.second = result[1] % MOD;
+  actual.third = result[2] % MOD;
   return actual;
 }
 
@@ -32,14 +58,19 @@ tuple hillCypherInvert (tuple actual) {
   #if DEBUG
     printf("Tuple in hillCypherInvert: %c%c%c\n", actual.first, actual.second, actual.third);
   #endif
-  productoMatrices((int *) &actual, calcularInversa(matrizKey, matrizKey, DIM), (int *) &actual, 1, 3, 3, 3);
+    int input[1][3] = {{actual.first, actual.second, actual.third}};
+  int * result = calloc(1*3, sizeof(int));
+  result = productoMatrices(input, matrizKeyInvserse, 1, 3, 3, 3);
+
+  actual.first = result[0] % MOD;
+  actual.second = result[1] % MOD;
+  actual.third = result[2] % MOD;
   return actual;
 }
 
-void desencrypt (char * input) {
+void decrypt (char * input) {
   int length = strlen(input) - 1; // -1 para no contar el fin de cadena \0
   printf("String length: %d\n", length);
-  
   for (int i = 0; i < length; i+=3) {
     tuple actual = {input[i], input[i+1], input[i+2]};
     tuple result = hillCypherInvert(actual);
@@ -50,9 +81,15 @@ void desencrypt (char * input) {
 }
 
 int main() {
+  #if DEBUG
+    puts("DEBUG MODE ACTIVATED");
+    printf("Matriz key: \n");
+    printMatrix(matrizKey, DIM, DIM);
+  #endif
+
   int option;
-  static char input[200];
-  static char * output;
+  char input[200];
+  char * output;
 
   puts("Hola bro!");
   puts("Si queres ingresar un mensaje para encriptar marca 0.");
@@ -61,12 +98,6 @@ int main() {
   printf("Elijo la opcion: ");
   scanf("%d", &option);
   fflush(stdin);
-
-  #if DEBUG
-    puts("DEBUG MODE ACTIVATED");
-    printf("Matriz llave: \n");
-    printMatrix(matrizKey, DIM, DIM);
-  #endif
 
   while (option != -1) {
     if (option == 0) {
@@ -84,7 +115,7 @@ int main() {
       fgets(input, 200, stdin);
       printf("Cadena ingresada: %s", input);
       puts("Desencriptando cadena...");
-      desencrypt(input);
+      decrypt(input);
       printf("Cadena desencriptada: %s\n", input);
 
     }
